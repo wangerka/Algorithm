c0f80f04dfee28528d2d735166ad09c6721d5836
gejun <917609824@qq.com>
2019-11-22 11:42:47 +0800

    bubble sort

---
+++ src/com/sort/BubbleSort.java
@@ -0,0 +1,27 @@
+package com.sort;
+
+public class BubbleSort {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] values = {4,23,2,7,12,34,22,3,6,1,100,35,0,99};
+		
+		for(int i=0;i<values.length;i++){
+			for(int j=0;j<values.length-1-i;j++){
+				if(values[j]>values[j+1]){
+					int tmp = values[j+1];
+					values[j+1]= values[j];
+					values[j] = tmp;
+				}
+			}
+		}
+
+		for(int i=0;i<values.length;i++){
+			System.out.print(values[i]+"->");
+		}
+	}
+
+}
