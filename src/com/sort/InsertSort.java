e4090370db5c26783e87d78869cd499562f0a575
gejun <917609824@qq.com>
2019-11-22 15:53:51 +0800

    insert sort

---
+++ src/com/sort/InsertSort.java
@@ -0,0 +1,19 @@
+package com.sort;
+
+public class InsertSort {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] values = {4,23,2,7,12,34,22,3,6,1,100,35,0,99};
+		int len = values.length;
+		//插入排序
+		for(int i=0;i<len;i++){
+			
+		}
+	}
+
+}
